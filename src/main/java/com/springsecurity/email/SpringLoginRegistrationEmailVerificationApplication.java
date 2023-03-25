cc15aa7fcc3787768df555663c1582368c33e4dc
Maycon Jardim <mayconsmill@gmail.com>
2023-03-25 19:44:42 -0300

    Projeto criado

---
+++ src/main/java/com/springsecurity/email/SpringLoginRegistrationEmailVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginRegistrationEmailVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginRegistrationEmailVerificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurity/email/SpringLoginRegistrationEmailVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginRegistrationEmailVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
