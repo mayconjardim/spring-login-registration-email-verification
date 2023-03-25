160d3075073ec6665a9cea05e450671b9d78a3d9
Maycon Jardim <mayconsmill@gmail.com>
2023-03-25 20:02:46 -0300

    User service implementando com UserDetailsService e com metodo para carrer usuario por email

---
+++ src/main/java/com/springsecurity/email/services/UserService.java
@@ -0,0 +1,24 @@
+package com.springsecurity.email.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.springsecurity.email.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		return userRepository.findByEmail(username)
+				.orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado!"));
+	}
+
+}
