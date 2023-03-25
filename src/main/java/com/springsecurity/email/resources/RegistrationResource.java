94ec7ec9c4919653b91ee759b8256c398b1857ed
Maycon Jardim <mayconsmill@gmail.com>
2023-03-25 20:11:57 -0300

    Registration Service e Resources criado

---
+++ src/main/java/com/springsecurity/email/resources/RegistrationResource.java
@@ -0,0 +1,25 @@
+package com.springsecurity.email.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springsecurity.email.entities.RegistrationRequest;
+import com.springsecurity.email.services.RegistrationService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("api/registration")
+@AllArgsConstructor
+public class RegistrationResource {
+
+	@Autowired
+	private RegistrationService registrationService;
+	
+	public String register(@RequestBody RegistrationRequest request) {
+		return registrationService.register(request);
+	}
+	
+}
---
+++ src/main/java/com/springsecurity/email/services/RegistrationService.java
@@ -0,0 +1,14 @@
+package com.springsecurity.email.services;
+
+import org.springframework.stereotype.Service;
+
+import com.springsecurity.email.entities.RegistrationRequest;
+
+@Service	
+public class RegistrationService {
+
+	public String register(RegistrationRequest request) {
+		return null;
+	}
+
+}
